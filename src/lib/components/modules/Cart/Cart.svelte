7e8c450e59b57d058b78420446aa8afbb225101e
Refael Blatt <mail@refael.ch>
2022-11-14 18:12:10 +0100

    setup: form wip

---
+++ src/lib/components/modules/Cart/Cart.svelte
@@ -0,0 +1,26 @@
+<script>
+    // export let cart;
+</script>
+
+<style lang="scss" src="./Cart.scss"></style>
+
+<form name="contact" method="POST" data-netlify="true">
+    <!-- <div class=”counter”>
+        <div class=”btn”>+</div>
+        <div class=”count”>2</div>
+        <div class=”btn”>-</div>
+    </div> -->
+    <p>
+      <label>Your Name: <input type="text" name="name" /></label>
+    </p>
+    <p>
+      <label>Your Email: <input type="email" name="email" /></label>
+    </p>
+    <!-- <p>
+      <label>Message: <textarea name="message"></textarea></label>
+    </p> -->
+    <p>
+      <button type="submit">Bestellen</button>
+    </p>
+  </form>
+  
--- src/lib/components/modules/Product/Product.svelte
+++ src/lib/components/modules/Product/Product.svelte
@@ -2,25 +2,39 @@
   
     export let products;
     const { fields } = products.props
-    // console.log("Products -->", fields)
+    console.log("Products -->", fields)
+    const { soldout } = fields
+    // console.log("soldOut -->", soldout)
 
     let title = "Bestellen"
 
 </script>
 
-<section>
+<style lang="scss" src="./Product.scss"></style>
+
+<section class="product">
 
     
     <h1>{title}</h1>
     
-    {#each fields as product}
-        <h3>{ product.productname }</h3>
-        <p>{ product.weight }g</p>
-        <p>{ product.description }</p>
-        <p>{ product.tags }</p>
-        <p>CHF { product.price }</p>
-        <p>max order amount { product.amount }</p>
-        <img src="{product.productphoto.url}" alt="">
-    {/each}
+    <div class="product__item">
+
+        {#each fields as product}
+            <h3 class="product__name">{ product.productname }</h3>
+            <p class="product__weight">{ product.weight }g</p>
+            <p class="product__description">{ product.description }</p>
+            <p class="product__tag">{ product.tags }</p>
+            <p class="product__price">CHF { product.price }</p>
+            <p>max order amount { product.amount }</p>
+            <img src="{product.productphoto.url}" alt="">
+            <!-- add Product to Cart.svelte -->
+            <button class="addProduct">
+                +
+            </button>
+                {#if product.soldout}
+                <p>sold out</p>
+                {/if}
+        {/each}
+    </div>
 
 </section>
