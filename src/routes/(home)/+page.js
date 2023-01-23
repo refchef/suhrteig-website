1d4ddc359255dc638c08c509ea1cb65100cb3492
Refael Blatt <mail@refael.ch>
2023-01-23 15:52:28 +0100

    test: ssr true, prerender false

--- src/routes/(home)/+page.js
+++ src/routes/(home)/+page.js
@@ -2,8 +2,8 @@ import query from "$lib/graphql/query/getHomepage.graphql"
 import {prismicQuery} from "$lib/util/prismic.js";
 
 // WHY?
-// export const ssr = false;
-export const prerender = true;
+export const ssr = true;
+export const prerender = false;
 
 // /** @type {import('./$types').PageServerLoad} */
 // /** @type {import('./$types').PageLoad} */
