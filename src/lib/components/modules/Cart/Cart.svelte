9a7a6f3556b0253fc2ccfe5979e8bc3c614b23ae
Refael Blatt <mail@refael.ch>
2023-01-28 22:40:54 +0100

    test: lowercase

--- src/lib/components/modules/Cart/Cart.svelte
+++ src/lib/components/modules/Cart/Cart.svelte
@@ -81,8 +81,8 @@
 	<div class="Cart__contact">
 		<p class="Cart__contact--title">{contact.title}</p>
 		<!-- NETLIFY FORM START -->
-		<form class="Cart__contact--form" name="Bestellungen" method="POST">
-			<input type="hidden" name="form-name" value="Bestellungen" />
+		<form class="Cart__contact--form" name="bestellungen" method="post">
+			<input type="hidden" name="form-name" value="bestellungen" />
 
 			<!-- NAME -->
 			<label for="name" class="Cart__contact--label"></label>
