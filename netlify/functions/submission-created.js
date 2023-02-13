4f7e92ddc9276b53aed02700148403fa12295443
Refael Blatt <mail@refael.ch>
2023-02-13 18:57:39 +0100

    feat: full submission test

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,33 +1,68 @@
-/*
-This will just respond with the OK status code, but you can expand this for your functionality.
-*/
-
-const {Client} = require("@notionhq/client")
+const { Client } = require("@notionhq/client");
 
 exports.handler = async function (event) {
 	const notion = new Client({ auth: process.env.NOTION_TOKEN });
-	const form = JSON.parse(event.body).payload.data
+	const form = JSON.parse(event.body).payload.data;
 	const new_row = {
 		parent: {
-		  database_id: process.env.DATABASE_ID,
+			database_id: process.env.DATABASE_ID,
 		},
 		properties: {
 			Name: {
-			  title: [
-				{
-				  text: {
-					content: form.name,
-				  },
-				},
-			  ],
+				title: [
+					{
+						text: {
+							content: form.name,
+						},
+					},
+				],
 			},
 			Email: {
-			  email: form.email,
+				email: form.email,
 			},
+			Address: {
+				title: [
+					{
+						text: {
+							content: form.address,
+						},
+					},
+				],
+			},
+			Order: {
+				title: [
+					{
+						text: {
+							content: form.order,
+						},
+					},
+				],
+			},
+			Price: {
+				number: {
+					format: franc,
+					content: form.price
+				},
+			},
+			Note: {
+				title: [
+					{
+						text: {
+							content: form.note,
+						},
+					},
+				],
+			},
+			Confirm: {
+				checkbox: form.confirm
+			},
+			Collect: {
+				checkbox: form.collect
+			}
 		},
-	}
+	};
 	await notion.pages.create(new_row);
 	return {
-	  statusCode: 200
-	}
-  }
+		statusCode: 200,
+	};
+};
