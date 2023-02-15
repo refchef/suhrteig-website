a3b59758ee7e942de6f4e3f75c94a847446dec36
Refael Blatt <mail@refael.ch>
2023-02-15 18:25:17 +0100

    test

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -20,42 +20,42 @@ exports.handler = async function (event) {
 			Email: {
 				email: form.email,
 			},
-			Address: {
-				rich_text: [
-					{
-						text: {
-							content: form.address
-						},
-					},
-				],
-			},
-			Order: {
-				rich_text: [
-					{
-					  text: {
-						content: form.order
-					  },
-					},
-				  ],
-			},
-			Price: {
-				number: form.price
-			},
-			Note: {
-				rich_text: [
-					{
-						text: {
-							content: form.note
-						},
-					},
-				],
-			},
-			Confirm: {
-				checkbox: form.confirm
-			},
-			Collect: {
-				checkbox: form.collect
-			}
+			// Address: {
+			// 	rich_text: [
+			// 		{
+			// 			text: {
+			// 				content: form.address
+			// 			},
+			// 		},
+			// 	],
+			// },
+			// Order: {
+			// 	rich_text: [
+			// 		{
+			// 		  text: {
+			// 			content: form.order
+			// 		  },
+			// 		},
+			// 	  ],
+			// },
+			// Price: {
+			// 	number: form.price
+			// },
+			// Note: {
+			// 	rich_text: [
+			// 		{
+			// 			text: {
+			// 				content: form.note
+			// 			},
+			// 		},
+			// 	],
+			// },
+			// Confirm: {
+			// 	checkbox: form.confirm
+			// },
+			// Collect: {
+			// 	checkbox: form.collect
+			// }
 		},
 	};
 	await notion.pages.create(new_row);
