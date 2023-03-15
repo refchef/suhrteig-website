aa96d7148708801ea2d459734cf7f39243a87012
Refael Blatt <mail@refael.ch>
2023-03-15 19:14:02 +0100

    try: serverless via netlify

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,64 +1,64 @@
-// const { Client } = require("@notionhq/client");
+const { Client } = require("@notionhq/client");
 
-// exports.handler = async function (event) {
-// 	const notion = new Client({ auth: process.env.NOTION_TOKEN });
-// 	const form = JSON.parse(event.body).payload.data;
-// 	const new_row = {
-// 		parent: {
-// 			database_id: process.env.DATABASE_ID
-// 		},
-// 		properties: {
-// 			Name: {
-// 				title: [
-// 					{
-// 						text: {
-// 							content: form.name
-// 						}
-// 					}
-// 				]
-// 			},
-// 			Email: {
-// 				email: form.email
-// 			},
-// 			Address: {
-// 				rich_text: [
-// 					{
-// 						text: {
-// 							content: form.address
-// 						}
-// 					}
-// 				]
-// 			},
-// 			Order: {
-// 				rich_text: [
-// 					{
-// 						type: text,
-// 						text: {
-// 							content: form.order
-// 						}
-// 					}
-// 				]
-// 			},
-// 			Price: {
-// 				number: form.total
-// 			},
-// 			Note: {
-// 				rich_text: [
-// 					{
-// 						type: text,
-// 						text: {
-// 							content: form.note
-// 						}
-// 					}
-// 				]
-// 			},
-// 			Confirm: {
-// 				checkbox: form.confirm
-// 			},
-// 			Collect: {
-// 				checkbox: form.collect
-// 			}
-// 		}
-// 	};
-// 	await notion.pages.create(new_row);
-// };
+exports.handler = async function (event) {
+	const notion = new Client({ auth: process.env.SECRET_NOTION_TOKEN });
+	const form = JSON.parse(event.body).payload.data;
+	const new_row = {
+		parent: {
+			database_id: process.env.SECRET_DATABASE_ID
+		},
+		properties: {
+			Name: {
+				title: [
+					{
+						text: {
+							content: form.name
+						}
+					}
+				]
+			},
+			Email: {
+				email: form.email
+			},
+			Address: {
+				rich_text: [
+					{
+						text: {
+							content: form.address
+						}
+					}
+				]
+			},
+			Order: {
+				rich_text: [
+					{
+						type: text,
+						text: {
+							content: form.order
+						}
+					}
+				]
+			},
+			Price: {
+				number: form.total
+			},
+			Note: {
+				rich_text: [
+					{
+						type: text,
+						text: {
+							content: form.note
+						}
+					}
+				]
+			},
+			Confirm: {
+				checkbox: form.confirm
+			},
+			Collect: {
+				checkbox: form.collect
+			}
+		}
+	};
+	await notion.pages.create(new_row);
+};
--- src/routes/(home)/+page.server.js
+++ src/routes/(home)/+page.server.js
@@ -1,86 +1,86 @@
-import { SECRET_DATABASE_ID } from "$env/static/private";
-import { SECRET_NOTION_TOKEN } from "$env/static/private";
+// import { SECRET_DATABASE_ID } from "$env/static/private";
+// import { SECRET_NOTION_TOKEN } from "$env/static/private";
 
-/** @type {import('./$types').Actions} */
-export const actions = {
-	default: async ({ event }) => {
-		const form = new FormData(event.target);
+// /** @type {import('./$types').Actions} */
+// export const actions = {
+// 	default: async ({ event }) => {
+// 		const form = new FormData(event.target);
 
-		const requestBody = {
-			"properties": {
-				"Name": {
-					"title": [
-						{
-							"text": {
-								"content": form.get("name")
-							},
-						},
-					],
-				},
-				"Email": {
-					"email": form.get("email"),
-				},
-				"Address": {
-					"rich_text": [
-						{
-							"text": {
-								"content": form.get("address")
-							},
-						},
-					],
-				},
-				"Order": {
-					"rich_text": [
-						{
-							"type": text,
-							"text": {
-								"content": form.get("order")
-							},
-						},
-					],
-				},
-				"Price": {
-					"number": form.get("total")
-				},
-				"Note": {
-					"rich_text": [
-						{
-							"type": text,
-							"text": {
-								"content": form.get("note")
-							},
-						},
-					],
-				},
-				"Confirm": {
-					"checkbox": form.get("confirm")
-				},
-				"Collect": {
-					"checkbox": form.get("collect")
-				},
-			}
-		};
+// 		const requestBody = {
+// 			"properties": {
+// 				"Name": {
+// 					"title": [
+// 						{
+// 							"text": {
+// 								"content": form.get("name")
+// 							},
+// 						},
+// 					],
+// 				},
+// 				"Email": {
+// 					"email": form.get("email"),
+// 				},
+// 				"Address": {
+// 					"rich_text": [
+// 						{
+// 							"text": {
+// 								"content": form.get("address")
+// 							},
+// 						},
+// 					],
+// 				},
+// 				"Order": {
+// 					"rich_text": [
+// 						{
+// 							"type": text,
+// 							"text": {
+// 								"content": form.get("order")
+// 							},
+// 						},
+// 					],
+// 				},
+// 				"Price": {
+// 					"number": form.get("total")
+// 				},
+// 				"Note": {
+// 					"rich_text": [
+// 						{
+// 							"type": text,
+// 							"text": {
+// 								"content": form.get("note")
+// 							},
+// 						},
+// 					],
+// 				},
+// 				"Confirm": {
+// 					"checkbox": form.get("confirm")
+// 				},
+// 				"Collect": {
+// 					"checkbox": form.get("collect")
+// 				},
+// 			}
+// 		};
 
-		const response = await fetch(
-			`https://api.notion.com/v1/databases/${SECRET_DATABASE_ID}/pages`,
-			{
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					"Notion-Version": "2021-05-13",
-					Authorization: `Bearer ${SECRET_NOTION_TOKEN}`,
-				},
-				body: JSON.stringify(requestBody),
-			}
-		);
+// 		const response = await fetch(
+// 			`https://api.notion.com/v1/databases/${SECRET_DATABASE_ID}/pages`,
+// 			{
+// 				method: "POST",
+// 				headers: {
+// 					"Content-Type": "application/json",
+// 					"Notion-Version": "2021-05-13",
+// 					Authorization: `Bearer ${SECRET_NOTION_TOKEN}`,
+// 				},
+// 				body: JSON.stringify(requestBody),
+// 			}
+// 		);
 
-		if (response.ok) {
-			console.log("New page created in Notion database!");
-		} else {
-			console.error(
-				"Error creating new page in Notion database:",
-				await response.text()
-			);
-		}
-	},
-};
+// 		if (response.ok) {
+// 			console.log("New page created in Notion database!");
+// 		} else {
+// 			console.error(
+// 				"Error creating new page in Notion database:",
+// 				await response.text()
+// 			);
+// 		}
+// 	},
+// };
