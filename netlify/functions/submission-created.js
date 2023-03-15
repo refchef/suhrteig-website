d762cf8fcf8ccca382132d8471286354946e68d1
Refael Blatt <mail@refael.ch>
2023-03-15 19:23:55 +0100

    try: new netlify function

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,64 +1,72 @@
 const { Client } = require("@notionhq/client");
 
 exports.handler = async function (event) {
-	const notion = new Client({ auth: process.env.SECRET_NOTION_TOKEN });
 	const form = JSON.parse(event.body).payload.data;
-	const new_row = {
-		parent: {
-			database_id: process.env.SECRET_DATABASE_ID
-		},
-		properties: {
-			Name: {
-				title: [
-					{
-						text: {
-							content: form.name
+	const notion = new Client({ auth: process.env.SECRET_NOTION_TOKEN });
+	try {
+		const new_row = {
+			parent: {
+				database_id: process.env.SECRET_DATABASE_ID
+			},
+			properties: {
+				Name: {
+					title: [
+						{
+							text: {
+								content: form.name
+							}
 						}
-					}
-				]
-			},
-			Email: {
-				email: form.email
-			},
-			Address: {
-				rich_text: [
-					{
-						text: {
-							content: form.address
+					]
+				},
+				Email: {
+					email: form.email
+				},
+				Address: {
+					rich_text: [
+						{
+							text: {
+								content: form.address
+							}
 						}
-					}
-				]
-			},
-			Order: {
-				rich_text: [
-					{
-						type: text,
-						text: {
-							content: form.order
+					]
+				},
+				Order: {
+					rich_text: [
+						{
+							type: text,
+							text: {
+								content: form.order
+							}
 						}
-					}
-				]
-			},
-			Price: {
-				number: form.total
-			},
-			Note: {
-				rich_text: [
-					{
-						type: text,
-						text: {
-							content: form.note
+					]
+				},
+				Price: {
+					number: form.total
+				},
+				Note: {
+					rich_text: [
+						{
+							type: text,
+							text: {
+								content: form.note
+							}
 						}
-					}
-				]
-			},
-			Confirm: {
-				checkbox: form.confirm
-			},
-			Collect: {
-				checkbox: form.collect
+					]
+				},
+				Confirm: {
+					checkbox: form.confirm
+				},
+				Collect: {
+					checkbox: form.collect
+				}
 			}
-		}
+		};
+		await notion.pages.create(new_row);
+	}
+	catch {
+		console.log(error)
+	}
+	return {
+		statusCode: 200,
 	};
-	await notion.pages.create(new_row);
 };
